52256612c4054cc9417158bf760c7d7ba0c827a2
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-13 17:48:31 +0100

    fetch profile, home and why pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
 import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
 
-export default function Home() {
+import { GetStaticProps } from 'next';
+import { groq } from 'next-sanity';
+import { fetchSanity } from './api/fetchSanity';
+
+import type { Profile } from '@/types/Profile';
+import type { Home } from '@/types/Home';
+import type { Why } from '@/types/Why';
+
+type Props = {
+  profile: Profile;
+  home: Home;
+  why: Why;
+}
+
+export default function Home(props: Props) {
+  // Object.keys(props).forEach(key => console.log(key, props[key]));
+
   return (
     <>
       <Head>
@@ -16,3 +32,29 @@ export default function Home() {
   )
 };
 
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  type PropsArray = [
+    Profile,
+    Home,
+    Why,
+  ];
+
+  const [
+    profile,
+    home,
+    why,
+  ]: PropsArray = await Promise.all([
+    fetchSanity(groq`*[_type == "profile"]{firstname, lastname, email, picture->}[0]`),
+    fetchSanity(groq`*[_type == "home"]{component->{name, page}, introductory, picture->}[0]`),
+    fetchSanity(groq`*[_type == "why"]{component->{name, page}, headTitle, shockPhrase, causes[] {name, icon->}}[0]`),
+  ])
+
+  return {
+    props: {
+      profile,
+      home,
+      why,
+    },
+    revalidate: 10
+  }
+};
