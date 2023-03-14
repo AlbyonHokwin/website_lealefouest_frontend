3160a88faf9a9445e4854eea12292c269acc41a5
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-14 12:25:57 +0100

    create all groq queries

---
+++ src/pages/api/groqQueries.ts
@@ -0,0 +1,14 @@
+import { groq } from 'next-sanity';
+
+export const groqQueries: Record<string, string> = {
+    components: groq`*[_type == "component"] | order(page, ASC) {name, page}`,
+    profile: groq`*[_type == "profile"]{firstname, lastname, email, picture->}[0]`,
+    home: groq`*[_type == "home"]{component->{name, page}, introductory, picture->}[0]`,
+    why: groq`*[_type == "why"]{component->{name, page}, headTitle, shockPhrase, causes[] {name, icon->}}[0]`,
+    solutions: groq`*[_type == "solution"]{component->{name, page}, headTitle, picture->, solutions, iconBonus->, bonuses}[0]`,
+    methodologies: groq`*[_type == "methodology"]{component->{name, page}, headTitle, methods[] {name, picture->}}[0]`,
+    pricing: groq`*[_type == "pricing"]{component->{name, page}, headTitle, price, motivation, icon->, reasons}[0]`,
+    expectations: groq`*[_type == "expectation"]{component->{name, page}, headTitle, expectations[] {text, icon->}}[0]`,
+    contact: groq`*[_type == "contact"]{component->{name, page}, headTitle, buttonText}[0]`,
+    otherMedias: groq`*[_type == "otherMedia"]{component->{name, page}, headTitle, arrowIcon->,medias[] {name, mainUrl, text, image->, useArrow, socialNetworks[] {name, icon->, url}}}[0]`,
+};
