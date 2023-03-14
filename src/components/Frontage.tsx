da39549df8a762e8554d961fc3aea08000e2ee9b
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-14 18:26:26 +0100

    finished component

--- src/components/Frontage.tsx
+++ src/components/Frontage.tsx
@@ -1,14 +1,49 @@
 import React from 'react';
 import styles from '@/styles/Frontage.module.css';
 
+import Image from './elements/ResponsiveImage';
+import { PortableText } from '@portabletext/react';
+
 import type { FrontageProps } from '@/types/FrontageProps';
 
-export default function Frontage(props: FrontageProps) {
-  const { introductory, picture} = props;
+type Props = FrontageProps & {
+  firstname: string;
+  lastname: string;
+}
+
+export default function Frontage(props: Props) {
+  const { introductory, picture, firstname, lastname } = props;
 
   return (
     <div className={styles.container}>
       <div className={styles.content}>
+        <div className={styles.left}>
+          <PortableText
+            value={introductory.text}
+            components={{
+              block: {
+                normal: ({ children }) => <h1 className={styles.catchPhrase}>{children}</h1>
+              },
+              marks: {
+                em: ({ children }) => <><em>{children}</em><br /></>
+              }
+            }}
+          />
+
+
+          <h2 className={styles.name}>
+            <div className={styles.whiteLine}>
+              <div className={styles.verticalLine}></div>
+            </div>
+            {firstname} {lastname}
+          </h2>
+        </div>
+
+        <div className={styles.right}>
+          <div className={styles.image}>
+            <Image image={picture} objectFit='cover' maxSize={1000} priority />
+          </div>
+        </div>
       </div>
     </div>
   );
