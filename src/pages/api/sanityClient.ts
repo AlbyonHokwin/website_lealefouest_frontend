ddc84239427f53817ffb503ee7bbd4697ce7a3f8
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-14 15:00:34 +0100

    change environment variable names

--- src/pages/api/sanityClient.ts
+++ src/pages/api/sanityClient.ts
@@ -2,8 +2,8 @@ import { createClient } from 'next-sanity';
 import type { SanityClient } from '@sanity/client';
 
 export const sanityClient: SanityClient = createClient({
-    projectId: process.env.SANITY_PROJECT_ID,
-    dataset: process.env.SANITY_DATASET,
-    apiVersion: process.env.SANITY_API_VERSION,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
     useCdn: false
 });
