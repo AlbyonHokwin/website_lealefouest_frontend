751ff347b228967aeaa6db21ae08ddb4e73f9543
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-15 18:50:35 +0100

    finish component

---
+++ src/components/Why.tsx
@@ -0,0 +1,42 @@
+import React, { ReactElement } from 'react'
+import styles from '@/styles/Why.module.css';
+
+import WhiteBand from './layouts/WhiteBand';
+import Cause from './elements/Cause';
+import { PortableText, PortableTextComponentProps, PortableTextBlockComponent } from '@portabletext/react';
+
+import type { WhyProps } from '@/types/WhyProps';
+
+type Props = WhyProps;
+
+export default function Why({ headTitle, shockPhrase, causes }: Props) {
+  return (
+    <WhiteBand headTitle={headTitle}>
+      <div className={styles.container}>
+
+        <div className={styles.shockPhraseContainer}>
+          <PortableText
+            value={shockPhrase.text}
+            components={{
+              block: {
+                normal: (props) => (
+                  <h4 className={styles.shockPhrase}>
+                    {props.value.children.map((child, i) => (
+                      <div key={i} className={child.marks.map((mark: string) => styles[mark])}>
+                        {child.text}
+                      </div>
+                    ))}
+                  </h4>
+                )
+              },
+            }}
+          />
+        </div>
+
+        <div className={styles.causes}>
+          {causes.map(cause => <Cause key={cause.name} {...cause} />)}
+        </div>
+      </div>
+    </WhiteBand>
+  );
+}
---
+++ src/components/elements/Cause.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from '@/styles/Cause.module.css';
+
+import Icon from './Icon';
+
+import type { Cause } from '@/types/WhyProps';
+
+type Props = Cause;
+
+export default function Cause({ name, icon }: Props) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.icon}>
+        <Icon icon={icon} objectFit='contain' />
+      </div>
+
+      <p className={styles.name}>
+        {name}
+      </p>
+    </div>
+  )
+}
