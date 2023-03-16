00aa6c2ceab7b7f514b215f9993ff7b71d431c9b
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-16 14:58:58 +0100

    add useEffect to apply filter

--- src/components/elements/Icon.tsx
+++ src/components/elements/Icon.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '@/styles/Icon.module.css'
 import NextImage from 'next/image';
 import imageUrlBuilder from '@sanity/image-url';
@@ -11,21 +11,29 @@ type Props = {
   icon: SanityIcon;
   objectFit: React.CSSProperties['objectFit'];
   useAccentColor?: boolean;
+  filter?: React.CSSProperties['filter'];
   priority?: boolean;
 }
 
-export default function Icon({ icon, objectFit, useAccentColor = false, priority = false }: Props) {
+export default function Icon({ icon, objectFit, useAccentColor = false, filter = '', priority = false }: Props) {
+  const [style, setStyle] = useState<React.CSSProperties>({ objectFit });
+
   const builder: ImageUrlBuilder = imageUrlBuilder(sanityClient);
 
   const src: string = builder.image(icon.icon)
     .url();
+
+  useEffect(() => {
+    useAccentColor && setStyle({ ...style, filter: 'var(--color-accent-filter)' });
+    filter && setStyle({ ...style, filter });
+  }, [useAccentColor, filter]);
 
   return (
     <div className={styles.container}>
       <NextImage
         src={src}
         alt={icon.alt}
-        style={{ objectFit, filter: useAccentColor ? 'var(--color-accent-filter)' : '' }}
+        style={style}
         fill
         sizes='100px'
       />
