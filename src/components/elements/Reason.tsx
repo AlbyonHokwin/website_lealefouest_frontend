e9e34567482cccb56c6beacecb11ed4747f5e237
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-16 10:55:57 +0100

    create component

---
+++ src/components/elements/Reason.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '@/styles/Reason.module.css';
+
+type Props = {
+  reason: string;
+};
+
+export default function Reason({ reason }: Props) {
+  return (
+    <li className={styles.container}>
+      {reason}
+    </li>
+  );
+}
---
+++ src/components/modules/Pricing.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styles from '@/styles/Pricing.module.css'
+
+import WhiteBand from '../layouts/WhiteBand';
+import Icon from '../elements/Icon';
+import Reason from '../elements/Reason';
+
+import type { PricingProps } from '@/types/PricingProps';
+
+type Props = PricingProps
+
+export default function Pricing({ headTitle, price, motivation, icon, reasons }: Props) {
+  return (
+    <WhiteBand headTitle={headTitle}>
+      <div className={styles.container}>
+        <div className={styles.priceContainer}>
+          <h4 className={styles.price}>
+            {price}€
+          </h4>
+
+          <div className={styles.divider} />
+
+          <p className={styles.motivation}>
+            {motivation}
+          </p>
+        </div>
+
+        <div className={styles.reasonsContainer}>
+          <div className={styles.iconContainer}>
+            <Icon icon={icon} objectFit='contain' useAccentColor />
+          </div>
+
+          <div className={styles.reasons}>
+            {reasons.map((reason, i) => <Reason key={i} reason={reason} />)}
+          </div>
+        </div>
+      </div>
+    </WhiteBand>
+  );
+}
