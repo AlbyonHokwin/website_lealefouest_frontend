a46bab7f7cf7854070547c91007831e71297743e
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-16 19:52:14 +0100

    add picture field

--- src/types/ContactProps.d.ts
+++ src/types/ContactProps.d.ts
@@ -1,9 +1,11 @@
 import type { Component } from './Component';
 import type { Comment } from './Comment';
+import type { SanityImage } from './SanityImage';
 
 export type ContactProps = {
     component: Component;
     headTitle: string;
+    picture: SanityImage;
     buttonText: string;
     comments?: Comment[];
 }
