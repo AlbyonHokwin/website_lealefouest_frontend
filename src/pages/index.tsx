246fb45454f4cd4b6eeb863c338824870c36d2cb
Camille HAUSTANT <camillehaustant@gmail.com>
2023-03-16 17:24:51 +0100

    add lang attribute

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -72,7 +72,7 @@ export default function Home(props: Props) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/icon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main lang='fr' className={styles.main}>
         <Background background={props.background} />
         {sections}
       </main>
